d795e79aea325e089f3192c45951781f324de5b4
William Sanz Vivanco <56824106+Aibloy@users.noreply.github.com>
2020-08-21 21:14:08 +0200

    Update IRIS_APP.py

--- IRIS_APP.py
+++ IRIS_APP.py
@@ -13,7 +13,6 @@ import streamlit as st
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import seaborn as sns 
-import os 
 from PIL import Image,ImageEnhance, ImageFilter
 #-------------------------------------------------------------
 #Title
